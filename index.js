88b40a72afd0e8e8894d0e9f7d8a59cf025d8293
asmovic <owoyemifood@gmail.com>
2020-03-11 13:26:02 +0100

    second commit

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ routes(app);
 
 const server = http.createServer(app);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`App running on port ${PORT}`)
 });
